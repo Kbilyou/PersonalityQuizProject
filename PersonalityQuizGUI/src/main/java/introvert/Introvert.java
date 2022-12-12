e640d3efd1fa2755c120ef3e2ac3ac6337cc598d
Kbilyou <120217736+Kbilyou@users.noreply.github.com>
2022-12-12 15:56:12 -0500

    Initial commit

---
+++ PersonalityQuizGUI/src/main/java/introvert/Introvert.java
@@ -0,0 +1,79 @@
+package introvert;
+
+import java.io.File;  
+import java.io.FileNotFoundException;  
+import java.util.Scanner; 
+
+// Team 4: Sheila Le
+public class Introvert 
+{
+	File file = new File("questions1.txt");
+	String[] questions = new String[40];
+	boolean read = false; // Read file or not
+	int index = 0;        // Index of position in questions.txt
+	int introvertPercent = 0, extrovertPercent = 0;
+	
+	public void readFile()
+	{
+		try (Scanner fileReader = new Scanner(file))
+		{
+			int i = 0;
+			
+			while (fileReader.hasNextLine()) 
+			{
+				questions[i] = fileReader.nextLine();
+				i++;
+		    }			
+		} 
+		catch (FileNotFoundException e) 
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public void getAnswer(int answer)
+	{
+		if (answer == 1) extrovertPercent += 10;
+		else if (answer == 2) introvertPercent += 10;
+	}
+	
+	public String printQuestions()
+	{
+		String question = "";
+		
+		for (int i = 0; i < 4; i++) 
+		{
+			question += "" + questions[index] + "\n";
+			index++;
+		}
+		question += "Please provide your answer\n";
+		
+		return question;
+	}
+	
+	public String intro()
+	{
+		if (!read) 
+		{
+			readFile();
+			read = true;
+		}
+		
+		return "This program processes will ask you a series of questions.\n"
+				+ "Your answers will be recorded to determine your percentage of being an introvert or extrovert.\n";
+	}
+	
+	public String result(int answer)
+	{
+		getAnswer(answer);
+		
+		return "Based off questions 1 to 10 you are " + introvertPercent + "% introvert and " + extrovertPercent + "% extrovert.";
+	}
+	
+	public String start(int answer) 
+	{
+		getAnswer(answer);
+		
+		return printQuestions();
+	}
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# PersonalityQuizProject
+ Personality Quiz is a java program that determines whether the user is an introvert or an extrovert.
